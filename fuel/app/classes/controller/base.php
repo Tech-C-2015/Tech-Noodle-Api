3d4c4861dc087adfa983d05b5d87b9cd03ff4c6e
y-ohgi <ohgi.211@gmail.com>
2016-01-03 00:08:51 +0900

    json_encode()の拡張メソッド、json_exencode()の追加

--- fuel/app/classes/controller/base.php
+++ fuel/app/classes/controller/base.php
@@ -1,8 +1,12 @@
 <?php
 class Controller_Base extends Controller_Hybrid
 {
+    protected $format = "";
 	public function before()
 	{
 		parent::before();
+        if($this->format === "json"){
+            $request->set_header('Content-Type', 'application/json');
+        }
 	}
 }
